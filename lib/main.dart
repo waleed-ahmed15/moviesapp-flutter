0b0ccda76d13c846c8037b5eb00357f62e2f6880
waleed-ahmed15 <waleed.ah755@gmail.com>
2023-05-29 20:47:14 +0500

    intiailzed app folder_structure

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:moviesapp/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(392.7, 803),
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeScreen(),
+        );
+      },
+    );
+
+    //  GetMaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+
+    //     primarySwatch: Colors.blue,
+    //   ),
+    //   home: const HomeScreen(),
+    // );
+  }
+}
